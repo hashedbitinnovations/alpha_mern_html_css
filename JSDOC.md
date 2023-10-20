78957ae5c0e8194fb3742279c0d66c0f7d0bc4ad
deepak104080 <deepak104080@gmail.com>
2023-10-21 00:13:16 +0530

    JS - Hoisting and Scope

--- JSDOC.md
+++ JSDOC.md
@@ -17,3 +17,25 @@ Varibales, Functions, Loops - https://jsfiddle.net/hashedbit/pLc8auys/1/
 IfElse Switch - https://jsfiddle.net/hashedbit/uxcgyhzm/2/
 Operators - https://jsfiddle.net/hashedbit/rLa5uy0n/2/
 Null, undefined and Not Defined - https://jsfiddle.net/hashedbit/8prLhvx3/2/
+
+
+Day 2
+Hoisting - test.js
+Scope - https://jsfiddle.net/hashedbit/7pLax1tu/1/
+Scope and Variable Shadowing - https://jsfiddle.net/hashedbit/neup0qjk/2/
+
+
+
+
+
+Function
+
+types of fn
+hof
+iife
+aarow
+
+
+
+coditionals detailed
+variable coercion
--- test.js
+++ test.js
@@ -1,2 +1,61 @@
+// var num;
+// var num2;
+// var num3;
+// function sum(num1, num2) {  //num1, num2 - parameters
+//     return num1 + num2
+// }
+// var sum2;
+
+// all var variables and function statements definition are moved to the top of the page. (Not their values or fn execution)
+////////////////////////////////////
+console.log(num);
+
 console.log('JavaScript Testing.....');
 console.log('JavaScript 2 Testing.....');
+
+var num = 5;
+
+console.log('JavaScript Testing.....');
+console.log('JavaScript 2 Testing.....');
+
+var num2 = 6;
+
+console.log('JavaScript Testing.....');
+console.log('JavaScript 2 Testing.....');
+
+console.log('JavaScript Testing.....');
+console.log('JavaScript 2 Testing.....');
+
+var num3 = 7;
+
+
+
+//////////////////////////////////////////////////
+//Function Statement
+
+//function definition
+function sum(num1, num2) {  //num1, num2 - parameters
+    return num1 + num2
+}
+
+//function calling
+sum(5,6) //5,6 - arguments
+
+
+//////////////////////////////////////////////////
+//function expression
+
+
+var sum2 = function abc(num1, num2) {
+    return num1 + num2
+}
+sum2(7,8)
+// abc(2,3)
+
+
+// arguments and parameters
+/////////////////////////////////////////
+//Anonymous function
+// function (num1, num2) {
+//     return num1 + num2
+// }
